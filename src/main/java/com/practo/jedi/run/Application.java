888cee3a1bfbab5e5096c5698f11861ff2b89c4d
kchetan <kasireddychetan@gmail.com>
2016-08-22 21:06:34 +0530

    Google Oauth simple

--- src/main/java/com/practo/jedi/run/Application.java
+++ src/main/java/com/practo/jedi/run/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // import log4j2test.MyClass;
@@ -16,10 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class Application {
-  @RequestMapping("/")
-  String home() {
-    return "Hello World!";
-  }
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
